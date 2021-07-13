e8fd955c04dd39032d3694859e7e1db10f9f5bef
Hannah Vernon <hannah@mvct.com>
2021-07-13 12:14:18 -0500

    genericzed connection string

--- ConnectionTimeoutTest/Program.cs
+++ ConnectionTimeoutTest/Program.cs
@@ -11,7 +11,7 @@ namespace ConnectionTimeoutTest
             {
                 InitialCatalog = "tempdb",
                 IntegratedSecurity = true,
-                DataSource = "Words"
+                DataSource = "localhost"
             };
 
             using SqlConnection connection = new SqlConnection();
